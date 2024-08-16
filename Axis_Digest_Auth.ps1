7cfee65226aef4df5c68bc14e26110668479db50
s3ptar <rene_graeber@graeber-elektronik.de>
2024-08-16 15:34:04 +0200

    update

---
+++ Axis_Digest_Auth.ps1
@@ -0,0 +1,3 @@
+﻿$password = ConvertTo-SecureString 'view' -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ('viewer', $password)
+Invoke-WebRequest "http://192.168.1.66/axis-cgi/param.cgi?action=list&group=Brand" -Credential $credential
